3e5d186e50afc500c6ff72973fffd0f5e7d6ef0a
Vinyl <38420904+VinylTheDuster@users.noreply.github.com>
2025-08-15 09:57:40 +0200

    fix for base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  base: '/cv-site-build/',
   vite: {
-    base: '/cv-site/',
     plugins: [tailwindcss()],
   },
 });
