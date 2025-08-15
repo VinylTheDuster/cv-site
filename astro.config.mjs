f10590a4279658a9948ef8599890225805fcf7b3
Vinyl <38420904+VinylTheDuster@users.noreply.github.com>
2025-08-15 10:21:16 +0200

    astro.config.mjs modifying + rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   base: '/cv-site-build/',
+  site: 'https://vinyltheduster.github.io/cv-site-build/',
   vite: {
     plugins: [tailwindcss()],
   },
